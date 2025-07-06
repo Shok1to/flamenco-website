c590f871b0b9f06c42e1078fe76fa75267674ccb
Shok1to <shoko.takahashi.911@gmail.com>
2025-07-05 21:34:08 -0400

    added aniomation

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,27 +1,20 @@
 "use client"
-import { useEffect, useState } from "react"
 
-export default function TestDisappear() {
-  const [show, setShow] = useState(true)
-  useEffect(() => {
-    const timer = setTimeout(() => setShow(false), 3000)
-    return () => clearTimeout(timer)
-  }, [])
+export default function EventsPage() {
   return (
     <div>
-      {show && (
-        <h1
-          className="fade-in-out"
-          style={{
-            background: "yellow",
-            color: "red",
-            textAlign: "center",
-            fontSize: "3rem",
-          }}
-        >
-          THIS SHOULD DISAPPEAR
-        </h1>
-      )}
+      <h1
+        style={{
+          animation: "fadeInOut 3s forwards",
+          willChange: "opacity",
+          background: "yellow",
+          color: "red",
+          fontSize: "3rem",
+          textAlign: "center",
+        }}
+      >
+        TEST FADE DIRECT STYLE
+      </h1>
       <div>Below test text</div>
     </div>
   )
